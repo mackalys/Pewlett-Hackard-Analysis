22067bbe4a4449d4b8d71ed7f9b66d3a779f33a8
MacKenzie Walters <mackenzie@walterstx.us>
2022-02-02 22:45:31 -0600

    adding retirement_info

--- schema.sql
+++ schema.sql
@@ -54,3 +54,17 @@ CREATE TABLE titles(
 );
 
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM dept_emp;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+--Retirement Eligibility
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1685-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
